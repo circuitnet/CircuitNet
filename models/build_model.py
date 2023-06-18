a4b7b57a7547ccfa6d77882b1fedce07cb09f3cc
apri0426 <apricity0426@gmail.com>
2023-06-18 15:58:39 +0800

    update

--- models/build_model.py
+++ models/build_model.py
@@ -8,9 +8,4 @@ def build_model(opt):
     model.init_weights(**opt)
     if opt['test_mode']:
         model.eval()
-    elif not opt['test_mode'] and opt['pretrained'] is not None:
-        if opt['cpu']:
-            model.load_state_dict(torch.load(opt['pretrained'], map_location=lambda storage, loc: storage)['state_dict'])
-        else:
-            model.load_state_dict(torch.load(opt['pretrained'])['state_dict'])
     return model
